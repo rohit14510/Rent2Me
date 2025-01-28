dc45aaa950570d779b59e535e902dd357d55a41b
Rohit Pancholi <rohitpancholi14510@gmail.com>
2025-01-28 18:44:44 +0530

    CHNGS

--- script.js
+++ script.js
@@ -8,5 +8,20 @@ document.addEventListener('DOMContentLoaded', function () {
         type: 'loop',
         perPage: 1,
         autoplay: true,
+        interval: 2000,
+    }).mount();
+});
+// Testimonials_splide Slider
+document.addEventListener('DOMContentLoaded', function () {
+    new Splide('#Testimonials_splide', {
+        type: 'loop',
+        perPage: 3, // Default for larger screens
+        autoplay: true,
+        interval: 2000, // Delay between slides (in milliseconds, e.g., 5000ms = 5 seconds)
+        breakpoints: {
+            600: { // Mobile view (you can adjust this value as needed)
+                perPage: 1, // Show 1 item on mobile
+            },
+        },
     }).mount();
 });
